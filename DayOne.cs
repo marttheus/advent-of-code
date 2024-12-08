3245db1f40211a6d16acd1182f6b2e9f478d0c3d
Matheus Felipe <marttheusf@gmail.com>
2024-12-08 00:54:26 -0300

    feat: aoc day one solution

---
+++ DayOne.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode;
+
+public static class DayOne
+{
+    public static int GetResponse(string[] input)
+    {
+        var numbers = GetNumbersOrdered(input);
+        
+        return GetTotalDistance(numbers);
+    }
+
+    private static (int, int)[] GetNumbersOrdered(string[] input)
+    {
+        var leftNumbers = new int [input.Length];
+        var rightNumbers = new int[input.Length];
+        
+        for (var i = 0; i < input.Length; i++)
+        {
+            var splitNumbers = input[i].Split("   ");
+
+            leftNumbers[i] = Convert.ToInt32(splitNumbers[0]);
+            rightNumbers[i] = Convert.ToInt32(splitNumbers[1]);
+        }
+
+        return leftNumbers
+            .OrderBy(x => x)
+            .Zip(rightNumbers.OrderBy(x => x), (leftNumber, rightNumber) => (leftNumber, rightNumber))
+            .ToArray();
+    }
+
+    private static int GetTotalDistance((int, int)[] numbers)
+    {
+        return GetDistances(numbers).Sum();
+    }
+
+    private static IEnumerable<int> GetDistances((int, int)[] numbers)
+    {
+        foreach (var (leftNumber, rightNumber) in numbers)
+        {
+            yield return GetDistance(leftNumber, rightNumber);
+        }
+    }
+    
+    private static int GetDistance(int leftNumber, int rightNumber) => Math.Abs(leftNumber - rightNumber);
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.IO;
+using AdventOfCode;
+
+var dayOneFile = "dayOneInput.txt";
+var dayOneInput = File.ReadAllLines(dayOneFile);
+var dayOneResponse = DayOne.GetResponse(dayOneInput);
+Console.WriteLine($"AoC Day One Response: {dayOneResponse}");
